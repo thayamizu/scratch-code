3115dea226c03e638f66bf75fce52eb3f0b7d234
tatsuya hayamizu <t.hayamizu@outlook.com>
2020-02-09 03:00:55 +0900

    create guessing-game

---
+++ rust/guessing-game/src/main.rs
@@ -0,0 +1,34 @@
+/**
+ * rust tutorial example 
+ * https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+ */
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop
+    {
+        let mut guess = String::new();
+        println!("Please input your guess.");
+        
+        io::stdin().read_line(&mut guess).expect("Failed to read input.");
+        let guess :u32 = match guess.trim().parse() {
+            Ok(val) => val,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            }
+        };
+    }
+    
+}
